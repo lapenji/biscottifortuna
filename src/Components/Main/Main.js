f1c1167f3008336a293991852304546b9bdee5dc
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-05-12 15:54:38 +0200

    modificato package.json per deploy su githubpages

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Consiglio from '../Consiglio/Consiglio';
 import styled from 'styled-components'
 import paper from './img/ricepaper.png'
