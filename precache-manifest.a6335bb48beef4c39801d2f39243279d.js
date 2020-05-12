bbd41e653ad0899b1c9faf2e67f495cbd4954f03
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-05-12 15:56:35 +0200

    Updates

---
+++ precache-manifest.a6335bb48beef4c39801d2f39243279d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adb8aa42da8ba8b82b92fc9b061249f",
+    "url": "/biscottifortuna/index.html"
+  },
+  {
+    "revision": "3b042480b6cd4761dc0e",
+    "url": "/biscottifortuna/static/css/main.f216e913.chunk.css"
+  },
+  {
+    "revision": "67520bb5f4602b128d2f",
+    "url": "/biscottifortuna/static/js/2.6e731a5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/biscottifortuna/static/js/2.6e731a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b042480b6cd4761dc0e",
+    "url": "/biscottifortuna/static/js/main.fdbdce85.chunk.js"
+  },
+  {
+    "revision": "db51323e32d3600a1e78",
+    "url": "/biscottifortuna/static/js/runtime-main.709d1bea.js"
+  },
+  {
+    "revision": "1bca4d1f824b6a7d974574e6c439e1d4",
+    "url": "/biscottifortuna/static/media/ricepaper.1bca4d1f.png"
+  }
+]);
